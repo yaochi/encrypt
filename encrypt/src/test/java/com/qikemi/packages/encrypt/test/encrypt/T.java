3eff2d293d3f825f6061bd5a09fe5300b28c35ae
xiexianbin <a.b@hotmail.com>
2014-10-08 23:01:25 +0800

    add junit test

---
+++ encrypt/src/test/java/com/qikemi/packages/encrypt/test/encrypt/T.java
@@ -0,0 +1,34 @@
+package com.qikemi.packages.encrypt.test.encrypt;
+
+import org.junit.Test;
+
+import com.qikemi.packages.encrypt.md5.MD5OrSSH1Encrypt;
+
+/**
+ * 
+ * Create Date: 2014年10月8日 下午10:40:59
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class T {
+
+	@Test
+	public void t1(){
+		
+		System.out.println(MD5OrSSH1Encrypt.md5("abcdef"));
+		// e80b5017098950fc58aad83c8c14978e
+		// e80b5017098950fc58aad83c8c14978e
+		
+		System.out.println(MD5OrSSH1Encrypt.sha1("abcdef"));
+		// 1f8ac10f23c5b5bc1167bda84b833e5c057a77d2
+		// 1f8ac10f23c5b5bc1167bda84b833e5c057a77d2
+		
+		System.out.println(MD5OrSSH1Encrypt.sha1AndMd5("abcdef"));
+		// 78cd8c52d92bc7c4e3808fbdb3434da4
+		// 78cd8c52d92bc7c4e3808fbdb3434da4
+		
+		System.out.println(MD5OrSSH1Encrypt.md5AndSha1("abcdef"));
+		// 3915da4d2d16dd5a69b6204cacb8a2a1d9e79f34
+		// 3915da4d2d16dd5a69b6204cacb8a2a1d9e79f34
+	}
+}
