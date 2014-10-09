b169e76c0698c4142fc9524b1a21d567cdecc6af
xiexianbin <a.b@hotmail.com>
2014-10-09 21:37:45 +0800

    update junit test

--- encrypt/src/test/java/com/qikemi/packages/encrypt/test/encrypt/T.java
+++ encrypt/src/test/java/com/qikemi/packages/encrypt/test/encrypt/T.java
@@ -3,7 +3,7 @@ package com.qikemi.packages.encrypt.test.encrypt;
 import org.junit.Test;
 
 import com.qikemi.packages.encrypt.base64encrypt.Base64Encrpt;
-import com.qikemi.packages.encrypt.md5.MD5OrSSH1Encrypt;
+import com.qikemi.packages.encrypt.md5.MD5OrSHA1Encrypt;
 
 /**
  * 
@@ -16,19 +16,19 @@ public class T {
 	@Test
 	public void t1(){
 		
-		System.out.println(MD5OrSSH1Encrypt.md5("abcdef"));
+		System.out.println(MD5OrSHA1Encrypt.md5("abcdef"));
 		// e80b5017098950fc58aad83c8c14978e
 		// e80b5017098950fc58aad83c8c14978e
 		
-		System.out.println(MD5OrSSH1Encrypt.sha1("abcdef"));
+		System.out.println(MD5OrSHA1Encrypt.sha1("abcdef"));
 		// 1f8ac10f23c5b5bc1167bda84b833e5c057a77d2
 		// 1f8ac10f23c5b5bc1167bda84b833e5c057a77d2
 		
-		System.out.println(MD5OrSSH1Encrypt.sha1AndMd5("abcdef"));
+		System.out.println(MD5OrSHA1Encrypt.sha1AndMd5("abcdef"));
 		// 78cd8c52d92bc7c4e3808fbdb3434da4
 		// 78cd8c52d92bc7c4e3808fbdb3434da4
 		
-		System.out.println(MD5OrSSH1Encrypt.md5AndSha1("abcdef"));
+		System.out.println(MD5OrSHA1Encrypt.md5AndSha1("abcdef"));
 		// 3915da4d2d16dd5a69b6204cacb8a2a1d9e79f34
 		// 3915da4d2d16dd5a69b6204cacb8a2a1d9e79f34
 	}
